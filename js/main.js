b81ec3542e6b7a9e6de0de498b74a8d1827324bf
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2021-12-11 19:28:11 +0100

    started making modal_card and has done crosshair to close modal, also created mixin for it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict'
+
+class Champagne {
+    #modalButtons = document.querySelectorAll('.card__modal-button')
+    #modalWindows = document.querySelector('#modal--3')
+    constructor() {
+        this.showModal();
+    }
+
+    showModal() {
+        this.#modalButtons.forEach(but => but.addEventListener('click', (e) => {
+            console.log('hello')
+            this.#modalWindows.classList.toggle('hidden')
+        }));
+    };
+};
+
+const champagne = new Champagne;
