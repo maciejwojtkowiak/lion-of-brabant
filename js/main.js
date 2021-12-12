5a05cbd7a6bfe5a97ac12a7b1cd8742633aa5d93
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2021-12-12 12:57:14 +0100

    finished modal logic in javascript

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ class Champagne {
     #modalButtons = document.querySelectorAll('.card__modal-button')
     #modalWindows = document.querySelector('.modal')
     #modalOverlay = document.querySelector('.overlay')
+    #activeModal;
     constructor() {
         this._showModal();
         this._hideModal();
@@ -11,23 +12,25 @@ class Champagne {
 
     _showModal() {
         this.#modalButtons.forEach(but => but.addEventListener('click', (e) => {
-            console.log('hello')
-            this._modalToggleClasses()
+            const clicked = e.target.closest('.card__modal-button');
+            if (!clicked) return;
+            this.#activeModal = document.querySelector(`.modal--${clicked.dataset.but}`);
+            this._modalToggleClasses();
         }));
     };
 
     _hideModal() {
         this.#modalOverlay.addEventListener('click', (e) => {
            if(e.target.closest('.overlay')) {
-            this._modalToggleClasses()
-           }
-        })
-    }
+            this._modalToggleClasses();
+           };
+        });
+    };
 
     _modalToggleClasses() {
         this.#modalOverlay.classList.toggle('hidden')
-        this.#modalWindows.classList.toggle('hidden')
-    }
+        this.#activeModal.classList.toggle('hidden')
+    };
 };
 
 const champagne = new Champagne;
