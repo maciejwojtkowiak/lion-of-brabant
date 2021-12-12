4b78a292ed1005e9066eccddeaf938b69b7b4617
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2021-12-12 23:31:09 +0100

    styled slide button

--- js/main.js
+++ js/main.js
@@ -29,33 +29,33 @@ class Champagne {
     };
 
     _modalToggleClasses() {
-        this.#modalOverlay.classList.toggle('hidden')
-        this.#activeModal.classList.toggle('hidden')
+        this.#modalOverlay.classList.toggle('hidden');
+        this.#activeModal.classList.toggle('hidden');
     };
 
     _slider() {
         const buttonBack = document.querySelector('.slider__previous');
-        const buttonForward = document.querySelector('.slider__next')
+        const buttonForward = document.querySelector('.slider__next');
         let currSlide = 0;
         const slideLength = this.#slides.length - 1
-        this.#slides.forEach((slide, i) => slide.style.transform = `translateX(${(i - currSlide) * 100}%)`)
+        this.#slides.forEach((slide, i) => slide.style.transform = `translateX(${(i - currSlide) * 100}%)`);
 
         buttonBack.addEventListener('click', (e) => {
             currSlide--
-            currSlide = currSlide < 0 ? currSlide = 0 : currSlide
-            this._moveToSlide(currSlide)
+            currSlide = currSlide < 0 ? currSlide = 0 : currSlide;
+            this._moveToSlide(currSlide);
             
         });
 
         buttonForward.addEventListener('click', (e) => {
             currSlide++
-            currSlide = currSlide > slideLength ? slideLength : currSlide
-            this._moveToSlide(currSlide)
+            currSlide = currSlide > slideLength ? slideLength : currSlide;
+            this._moveToSlide(currSlide);
         });
     };
 
     _moveToSlide(slide) {
-        this.#slides.forEach((sli, i) => sli.style.transform = `translateX(${(i - slide) * 100}%)`) 
+        this.#slides.forEach((sli, i) => sli.style.transform = `translateX(${(i - slide) * 100}%)`) ;
     }
 };
 
