334f59718679092aaa29d12a136a123954a47864
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2021-12-12 11:59:49 +0100

    made closing modal window

--- js/main.js
+++ js/main.js
@@ -2,17 +2,32 @@
 
 class Champagne {
     #modalButtons = document.querySelectorAll('.card__modal-button')
-    #modalWindows = document.querySelector('#modal--3')
+    #modalWindows = document.querySelector('.modal')
+    #modalOverlay = document.querySelector('.overlay')
     constructor() {
-        this.showModal();
+        this._showModal();
+        this._hideModal();
     }
 
-    showModal() {
+    _showModal() {
         this.#modalButtons.forEach(but => but.addEventListener('click', (e) => {
             console.log('hello')
-            this.#modalWindows.classList.toggle('hidden')
+            this._modalToggleClasses()
         }));
     };
+
+    _hideModal() {
+        this.#modalOverlay.addEventListener('click', (e) => {
+           if(e.target.closest('.overlay')) {
+            this._modalToggleClasses()
+           }
+        })
+    }
+
+    _modalToggleClasses() {
+        this.#modalOverlay.classList.toggle('hidden')
+        this.#modalWindows.classList.toggle('hidden')
+    }
 };
 
 const champagne = new Champagne;
